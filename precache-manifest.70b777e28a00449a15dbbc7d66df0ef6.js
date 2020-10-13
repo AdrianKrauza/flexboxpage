3e16f56000ade7425e130b2306fd149df276e540
AdrianKrauza <45629093+AdrianKrauza@users.noreply.github.com>
2020-10-13 12:23:35 +0200

    Add files via upload

---
+++ precache-manifest.70b777e28a00449a15dbbc7d66df0ef6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d8cc0329348bbff9b356e8e28577d3e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0ec8b6cd4b1b07fe72fd",
+    "url": "./static/js/2.42e62f34.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "./static/js/2.42e62f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96093fa7078d72041be7",
+    "url": "./static/js/main.5fc3a9dd.chunk.js"
+  },
+  {
+    "revision": "3161687a306374ab5740",
+    "url": "./static/js/runtime-main.b9af2473.js"
+  },
+  {
+    "revision": "c47510fe866525188c61d921d56b1b25",
+    "url": "./static/media/arrow.c47510fe.svg"
+  },
+  {
+    "revision": "c748de6bc381ca87c6c03e7748d0c9fe",
+    "url": "./static/media/close.c748de6b.svg"
+  }
+]);
